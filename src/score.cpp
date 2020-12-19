a4823ced0a17fb90e4ab5a659a402b792f870fdc
JusufAmg <jamgad23@gmail.com>
2020-12-19 22:34:40 +0300

    Update score.cpp

--- src/score.cpp
+++ src/score.cpp
@@ -11,6 +11,8 @@ Score::Score(std::string file_name) : File_Name(file_name)  // good
 {
 	
 }
+
+// Opens the high_sores.txt file,or creates the file if it is doesn't exist and saves the data in scores
 void Score::Data(){
 std::ifstream input(File_Name);
 	if (!input) {
@@ -27,12 +29,13 @@ std::ifstream input(File_Name);
 	}
 }
 
+// Adds current game score and sorts the scores 
 void Score::Add_score(std::string name, int score){ //good 
 	scores.push_back(std::pair<std::string, int>(name, score));
 	Sort(scores);
 }
 
-void Score::Write() { //good 
+void Score::Write() { // Adds the new score to high_scores.txt 
 	std::ofstream output(File_Name, std::ofstream::out);
 	for (auto x : scores) {
 		output << x.first << " " << x.second << "\n";
@@ -49,6 +52,7 @@ void Sort(std::vector<std::pair<std::string,int>> &scores)
 	std::sort(scores.begin(), scores.end(), cmp);
 }
 
+// prints scores in terminal after the game is over
 void Score::Print_scores() //good
 {
 	int i = 1;
@@ -59,4 +63,3 @@ void Score::Print_scores() //good
 		i++;
 	}
 }
-
