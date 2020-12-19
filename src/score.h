54c89853c3194bd2d4952b2e4256fd43c0afe4f6
JusufAmg <jamgad23@gmail.com>
2020-12-19 09:38:31 +0300

    Update score.h

--- src/score.h
+++ src/score.h
@@ -2,25 +2,22 @@
 #define SCORE
 
 #include <string>
-#include <map>
+#include <vector>
 
 class Score
 {
 public:
 	Score(std::string file_name);
-	bool Available();
-	std::string Get_file();
-	void Create_file();
 	void Add_score(std::string name, int score);
 	void Print_scores();
 	void Write();
-	void Prev_Scores();
+	void Data();
 
 private:
 	std::string  File_Name;
-	std::multimap<std::string, int> scores;
+	std::vector<std::pair<std::string,int>> scores;
 };
-	void Sort(std::multimap<std::string, int> &scores);
+	void Sort(std::vector<std::pair<std::string,int>> &scores);
 
 
 #endif
